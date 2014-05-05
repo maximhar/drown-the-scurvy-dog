7dfa179460bbf0486421b19584471508cca49a3a
Maxim Harizanov <maxim.harizanov@gmail.com>
2014-05-05 08:42:33 +0100

    Final

--- src/uk/ac/aber/dcs/msh4/cs124/gui/GameFrame.java
+++ src/uk/ac/aber/dcs/msh4/cs124/gui/GameFrame.java
@@ -1,29 +1,57 @@
 package uk.ac.aber.dcs.msh4.cs124.gui;
 
+import uk.ac.aber.dcs.msh4.cs124.model.PirateHangman;
+
 import javax.swing.*;
+import javax.swing.Timer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.*;
 
 public class GameFrame extends JFrame {
+    JPanel mainPanel;
     private ActionListener wordListener;
     private ActionListener symbolListener;
     private JLabel statusLabel;
-    private JPanel animationPanel;
-
+    private PiratePanel animationPanel;
+    private Timer wavesTimer;
+    private PirateHangman model;
+    private JTextField wordField;
+    private JTextField symbolField;
     public GameFrame(){
         init();
     }
 
     private void init() {
+
         setTitle("Drown the Scurvy Dog");
         setLocationRelativeTo(null);
-        setSize(400, 300);
+        setSize(500, 300);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        
+        try {
+            model = new PirateHangman(10);
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Failed to load pirate dictionary, closing");
+            this.setVisible(false);
+            return;
+        }
+        wavesTimer = new Timer(500, new ActionListener() {
+            boolean waveHigh = false;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                waveHigh = !waveHigh;
+                animationPanel.setShipVerticalOffset(waveHigh ? -0.01f : 0.01f);
+                animationPanel.repaint();
+            }
+        });
+        wavesTimer.start();
+
         createListeners();
         
-        JPanel mainPanel = new JPanel(new GridBagLayout());
+        mainPanel = new JPanel(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         //extremely verbose code creating the main layout...
         c.weightx = 0.5;
@@ -40,13 +68,13 @@ public class GameFrame extends JFrame {
         c.gridy = 1;
         mainPanel.add(label, c);
 
-        JTextField wordField = new JTextField();
+        wordField = new JTextField();
         c.gridx = 1;
         c.gridy = 0;
         c.weightx = 2;
         mainPanel.add(wordField, c);
 
-        JTextField symbolField = new JTextField();
+        symbolField = new JTextField();
         c.gridx = 1;
         c.gridy = 1;
         c.weightx = 2;
@@ -80,24 +108,77 @@ public class GameFrame extends JFrame {
         c.weightx = 0.5;
         c.gridwidth = 3;
         c.fill = GridBagConstraints.BOTH;
+        animationPanel.setPiratePositions(model.guessLeft());
         mainPanel.add(animationPanel, c);
 
         getContentPane().add(mainPanel);
+        updateStatus();
     }
-
+    private void reinit(){
+        this.getContentPane().remove(mainPanel);
+        init();
+    }
     private void createListeners() {
         this.wordListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                if(wordField.getText().length() < 1) return;
+                if(model.tryWord(wordField.getText())) {
+                    tellVictory();
+                } else if (model.guessLeft() <= 0){
+                    tellLoss();
+                } else {
+                    updateStatus();
+                }
             }
         };
 
         this.symbolListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                if(symbolField.getText().length() < 1) return;
+                if(model.tryThis(symbolField.getText().charAt(0))) {
+                    tellVictory();
+                } else if (model.guessLeft() <= 0){
+                    tellLoss();
+                } else {
+                    updateStatus();
+                }
             }
         };
+    }
+
+    private void updateStatus() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                statusLabel.setText("You can see: " + model.getVisible() + "; Letters tried: " + model.getLetters() + "; Guesses left: " + model.guessLeft());
+                animationPanel.setPiratePosition(animationPanel.getPiratePositions() - model.guessLeft());
+                animationPanel.repaint();
+                symbolField.setText("");
+                wordField.setText("");
+            }
+        });
+    }
+
+
+    private void tellVictory(){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JOptionPane.showMessageDialog(null, "You won! The word was " + model.getHidden());
+                reinit();
+            }
+        });
+    }
+
+    private void tellLoss(){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JOptionPane.showMessageDialog(null, "You lost! The word was " + model.getHidden());
+                reinit();
+            }
+        });
     }
 }
--- src/uk/ac/aber/dcs/msh4/cs124/gui/PiratePanel.java
+++ src/uk/ac/aber/dcs/msh4/cs124/gui/PiratePanel.java
@@ -6,10 +6,38 @@ import java.awt.*;
 public class PiratePanel extends JPanel {
     private int piratePositions = 10;
     private int piratePosition = 0;
+    private float shipH = 0.3f;
+    private float shipW = 0.2f;
+    private float shipX = 0.7f;
+    private float shipY = 0.3f;
+    private float plankLen = 0.5f;
+    private float pirateHeight = 0.2f;
+    private float pirateWidth = 0.1f;
+
 
     @Override
     public void paintComponent(Graphics g){
+        // clear the whole panel
+        g.setColor(Color.WHITE);
+        g.fillRect(0,0,scaleX(1), scaleY(1));
+        // then draw the scene
         drawOcean(g);
+        drawShip(g);
+        drawPlank(g);
+        drawPirate(g);
+    }
+
+    private void drawPirate(Graphics g) {
+        g.setColor(Color.RED);
+        float pirateX =  + shipX - ((float) getPiratePosition() / (float) getPiratePositions()) * plankLen;
+        float pirateY = shipY + shipH/2 - pirateHeight;
+        g.fillOval(scaleX(pirateX), scaleY(pirateY), scaleX(pirateWidth), scaleY(pirateHeight));
+        g.drawString("P", scaleX(pirateX), scaleY(pirateY));
+    }
+
+    private void drawPlank(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.drawLine(scaleX(shipX), scaleY(shipY + shipH/2), scaleX(shipX - plankLen), scaleY(shipY + shipH/2));
     }
 
     private void drawOcean(Graphics g) {
@@ -17,11 +45,38 @@ public class PiratePanel extends JPanel {
         g.fillRect(scaleX(0), scaleY(0.5f), scaleX(1), scaleY(0.5f));
     }
 
+    private void drawShip(Graphics g){
+        g.setColor(Color.BLACK);
+        g.fillPolygon(new int[] { scaleX(shipX), scaleX(shipX + shipW/2), scaleX(shipX + shipW)},
+                new int[] { scaleY(shipY + shipH/2), scaleY(shipY + shipH), scaleY(shipY  + shipH/2)}, 3);
+        g.drawLine(scaleX(shipX + shipW/2), scaleY(shipY), scaleX(shipX + shipW/2), scaleY(shipY + shipH/2));
+    }
+    /// Scales a relative x coordinate to an absolute x coordinate
     private int scaleX(float x){
         return (int)(x * this.getWidth());
     }
-
+    /// Scales a relative y coordinate to an absolute y coordinate
     private int scaleY(float y){
         return (int)(y * this.getHeight());
+    }
+    /// The count of positions the pirate may take between the start of the plank and him drowning
+    public int getPiratePositions() {
+        return piratePositions;
+    }
+    /// Sets the count of positions the pirate may take between the start of the plank and him drowning
+    public void setPiratePositions(int piratePositions) {
+        this.piratePositions = piratePositions;
+    }
+    /// Gets the current position of the pirate
+    public int getPiratePosition() {
+        return piratePosition;
+    }
+    /// Sets the position of the pirate on the plank
+    public void setPiratePosition(int piratePosition) {
+        this.piratePosition = piratePosition;
+    }
+    /// Sets the vertical offset of the ship
+    public void setShipVerticalOffset(float f){
+        this.shipY += f;
     }
 }
